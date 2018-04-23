f254aa58ff5165ad7669cd80b30ea2803569fe8e
DenisTurkovic <d.turkovic00@gmx.ch>
2018-04-23 15:41:15 +0200

    Creating Project...

---
+++ src/mvc/model/TextVerschluesseln.java
@@ -0,0 +1,209 @@
+package mvc.model;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class TextVerschluesseln implements StdFilesLoc{
+	
+	/**
+	 * Klassenvariablen
+	 */
+	private String filename;
+	private String text = "";
+	private String verschluesselterText = "";
+	private String decodierterText = "";
+	private String pwd;
+	int zpwd;
+	int auswahl;
+	private char[] charArray = {};
+	private char[] pwdArray = {};
+	boolean testing = true;
+	int testMethod = 0;
+	int testergebnis = 0;
+	Scanner scan = new Scanner(System.in);
+	
+	/**
+	 * Konstruktor mit @param testing Parameter
+	 */
+	public TextVerschluesseln(boolean testing){
+		this.testing = testing;
+		if(testing){
+			System.out.println("\n###########################");
+			System.out.println("Testing Textverschlüsselung");
+			System.out.println("###########################\n");
+			pwd = "test";
+			filename = "versch.txt";
+			auswahl = 1;
+			eingabe();
+			testMethod++;
+			text = "Dies ist ein Test Text";
+			auswahl = 2;
+			verschluesselterText = "";
+			decodierterText = "";
+			zpwd = 0;
+			eingabe();
+		}else{
+			eingabe();
+		}
+	}
+	/**
+	 * Konstruktor mit @param text Parameter
+	 */
+	public TextVerschluesseln(String text){
+		this.text = text;
+		verschluesseln(text);
+	}
+	/**
+	 * Menuefuehrung mit Usereingabe und File lesen
+	 */
+	public void eingabe(){
+		if(testing == false){
+			System.out.println("Möchten Sie eine Eingabe über ein Text file oder per Tastatur Eingabe? 1 für File, 2 für Tastatur");
+			auswahl = scan.nextInt();
+		}		
+		if(auswahl == 1){
+			if(testing == false){
+				System.out.println("Bitte speichern Sie das File im input Ordner ab und geben Sie den Filename an. Beispiel: text.txt");
+				filename = scan.next();
+			}
+			File mytext = new File(input.toString(), filename);
+			
+			try {
+				BufferedReader br = new BufferedReader(new FileReader(mytext));
+				String line;
+			    while ((line = br.readLine()) != null) {
+	                System.out.println(line);
+	                text +=  line;
+	            }
+	        } catch (IOException e) {
+	            System.out.println("File existiert nicht");
+	            eingabe();
+	        } 
+			
+			verschluesseln(text);
+		}
+		
+		if (auswahl == 2){
+			if(testing == false){
+				System.out.println("Bitte geben Sie den zu verschlüsselnden text ein\n");
+				scan.nextLine();
+				text = scan.nextLine();
+			}
+			verschluesseln(text);
+		}
+		if(auswahl > 2 || auswahl < 1){
+			System.out.println("falsche Eingabe, versuchen Sie es nochmals");
+			eingabe();
+		}
+		
+	}
+	/**
+	 * Methode verschluesseln mit @param Text
+	 */
+	public void verschluesseln(String Text){
+		if(testing == false){
+			System.out.println("Bitte geben Sie das Passwort für die Verschlüsselung ein");
+			pwd = scan.next();
+		}
+		
+		pwdArray = pwd.toCharArray();
+		charArray = text.toCharArray();
+		
+		for(int i = 0; i < charArray.length; i++){
+			charArray[i] = (char) (charArray[i] + pwdArray[zpwd]);
+			zpwd++;
+			
+			if(zpwd == pwd.length()){
+				zpwd = 0;
+			}
+		}
+		
+		for(int i = 0; i < charArray.length; i++){
+			verschluesselterText += charArray[i];
+		}
+		ausgabe(verschluesselterText, 1);
+		decoder(charArray);
+	}
+	/**
+	 * Methode decoder mit @param charArray
+	 */
+	private void decoder(char[] charArray){
+		zpwd = 0;
+		for(int i = 0; i < charArray.length; i++){
+			charArray[i] = (char) (charArray[i] - pwdArray[zpwd]);
+			zpwd++;
+			
+			if(zpwd == pwd.length()){
+				zpwd = 0;
+			}
+		}
+		for(int i = 0; i < charArray.length; i++){
+			decodierterText += charArray[i];
+		}
+		ausgabe(decodierterText, 2);
+	}
+	/**
+	 * Methode ausgabe mit:
+	 * @param ausgabe
+	 * @param check
+	 */
+	private void ausgabe(String ausgabe, int check) {
+		if(check == 1){
+			System.out.println("Verschlüsselter Text: " + ausgabe);
+		}
+		else{
+			System.out.println("decodierter Text: " + ausgabe);
+			if(testing){
+				test();
+			}
+		}	
+	}
+	/**
+	 * Methode test
+	 */
+	private void test(){
+		if(testMethod == 0){
+			if(decodierterText.equals(text)){
+				System.out.println("\nVer- und entschlüsselung mit File erfolgreich!\n");
+				testergebnis++;
+			}else{
+				System.out.println("\nVer- und entschlüsselung mit File fehlerhaft!\n");
+			}
+		}else{
+			if(decodierterText.equals(text)){
+				System.out.println("\nVer- und entschlüsselung mit Eingabe erfolgreich!\n");
+				testergebnis++;
+				if(testergebnis == 2){
+					System.out.println("\nDer Test konnte erfolgreich abgeschlossen werden!");
+				}
+				else{
+					System.out.println("\nBitte Fehler überprüfen!");
+				}
+			}else{
+				System.out.println("Ver- und entschlüsselung mit Eingabe fehlerhaft!");
+				System.out.println("\nBitte Fehler überprüfen!");
+			}
+		}
+	}
+
+
+	/**
+	 * Methoden welche vom Interface StdFilesLoc uebernommen werden.
+	 */
+	@Override
+	public boolean check_folder() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean create_folder() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/mvc/model/TicTacToe.java
@@ -0,0 +1,211 @@
+package mvc.model;
+import java.util.Scanner;
+
+public class TicTacToe {
+
+	/**
+	 * Klassen Variablen
+	 * 
+	 * Variablen welche eine Erklaerung brauchen
+	 * private boolean turn = Bei True ist player 1 dran & false player 2
+	 * private boolean gameon = Bei True ist das Spiel noch am laufen, Bei False ist das Spiel vorbei.
+	 * */
+	private String[][] field = new String[3][3];
+	private String player1 = "o";
+	private String player2 = "x";
+	private boolean turn;
+	private Scanner scan;
+	private boolean gameon;
+	
+	/**
+	 * Konstruktor welcher aufgerufen wird um das Spiel aufzusetzen
+	 * */
+	public TicTacToe() {
+		this.scan = new Scanner(System.in);
+		this.turn = true;
+		this.gameon = true;
+		initGame();	
+	}
+	/**
+	 * Das Spielfeld wird mit lauter Leerzeichen aufgefuellt
+	 * */
+	private void initGame() {
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field.length; j++) {
+				this.field[i][j] = " ";
+			}
+		}
+		
+	}
+	/**
+	 * Um das Spiel zu starten diese Methode aufrufen.
+	 * Diese Methode laeuft solange bis es einen Sieger hat
+	 * */
+	public void start() {
+		do {
+			user_input();
+		} while (gameon);
+		System.out.println("The game is over");
+	}
+	/**
+	 * Methode fragt nach User Input (Entweder Player 1 oder Player 2).
+	 * Falls die Methode setfield ein False zurueck gibt, wird ausgegeben
+	 * das ein ungueltiger Spielzug stattgefunden hat & der Spieler kann aber immernoch
+	 * setzen.
+	 * */
+	private void user_input() {
+		System.out.println(turn ? "Player 1" : "Player 2");
+		System.out.println("Give in Row - (1-3) and Column | (1-3)");
+		int row = this.scan.nextInt();
+		int column = this.scan.nextInt();
+		if(setfield(row, column)) {
+			draw_field();
+			check_win();
+		} else {
+			System.out.println("Ungültiger Spielzug!");
+		}
+		if(full_field()) {
+			this.gameon = false;
+		}
+	}
+	/**
+	 * Setzt den "Stein", x oder o, auf das leere Feld.
+	 * Falls das Feld schon besetzt ist oder eine ungültige Eingabe folgt
+	 * gibt die Methode ein false
+	 * */
+	private boolean setfield(int column, int row) {
+		if(row > 3 || column > 3)
+			return false;
+		--row;
+		--column;
+		if(this.field[row][column] == " "){
+			if(turn) {
+				this.field[row][column] = player1;
+				this.turn = false;
+			} else {
+				this.field[row][column] = player2;
+				this.turn = true;
+			}
+				
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Das Spielfeld wird auf Konsolenlevel ausgegeben
+	 * */
+	private void draw_field(){
+		for (int i = 0; i < field.length; i++) {
+			for (int j = 0; j < field.length; j++) {
+				System.out.print(this.field[i][j]);
+				System.out.print(" | ");
+			}
+			System.out.println();
+			System.out.print("- - - - - -");
+			System.out.println();
+		}
+	}
+	
+	/**
+	 * Prueft ob ein Spieler gewonnen hat.
+	 * Da es nur 9 Moeglichkeiten gibt um zu gewinnen (was eher wenig sind)
+	 * sind diese im switch case.
+	 * Es wird ein String mit den Felder gebildet und der Methode check_line weitergegeben
+	 * */
+	private void check_win(){
+		String line = "";
+		for (int i = 0; i < 9; i++) {
+			switch (i) {
+			case 0:
+				line = this.field[0][0] + this.field[1][0] + this.field[2][0];
+				break;
+			case 1:
+				line = this.field[1][0] + this.field[1][1] + this.field[1][2];
+				break;
+			case 2:
+				line = this.field[2][0] + this.field[2][1] + this.field[2][2];
+				break;
+			case 3:
+				line = this.field[0][0] + this.field[0][1] + this.field[0][2];
+				break;
+			case 4:
+				line = this.field[0][0] + this.field[1][0] + this.field[2][0];
+				break;
+			case 5:
+				line = this.field[1][0] + this.field[1][1] + this.field[1][2];
+				break;
+			case 6:
+				line = this.field[2][0] + this.field[2][1] + this.field[2][2];
+				break;
+			case 7:
+				line = this.field[0][0] + this.field[1][1] + this.field[2][2];
+				break;
+			case 8:
+				line = this.field[0][2] + this.field[1][1] + this.field[2][0];
+				break;
+
+			default:
+				break;
+			}
+			if(check_line(line)) {
+				System.out.println("We have a Winner");
+				this.gameon = false;
+				break;
+			}
+				
+		}
+		
+	}
+	/**
+	 * Prueft den String line ob dieser "xxx" oder "ooo" ist und 
+	 * gibt true als Rueckgabewert
+	 * */
+	private boolean check_line(String line) {
+		if (line.equals("xxx")) {
+			return true;
+		} else if(line.equals("ooo")){
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * Prüft ob das Spielfeld voll ist
+	 * @return
+	 */
+	private boolean full_field() {
+		boolean gameover = true;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if(this.field[i][j].equals(" ")) {
+					gameover = false;
+				}
+			}
+		}
+		return gameover;
+	}
+	
+	/**
+	 * Kleine Debug Methode um das Spielfeld zu prüfen.
+	 * */
+	public void debug() {
+		debug_field();
+		draw_field();
+		check_win();
+		System.out.println(this.gameon ? "Nothing" : "Win");;
+	}
+	/**
+	 * Diese Methode fuellt das Spielfeld aus.
+	 * */
+	private void debug_field(){
+		this.field[2][0] = "x";
+		this.field[1][0] = "o";
+		this.field[0][2] = "x";
+		this.field[1][2] = "o";
+		this.field[1][1] = "x";
+
+	}
+}
