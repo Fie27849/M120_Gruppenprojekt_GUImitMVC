795e30597714e47e1b5006613254964d1515e63e
Sandro <sandro.trottmann@casasoft.ch>
2018-04-23 16:15:03 +0200

    asd

--- src/mvc/controller/Controller.java
+++ src/mvc/controller/Controller.java
@@ -1,18 +1,23 @@
 package mvc.controller;
 
+import mvc.model.TextVerschluesseln;
 import mvc.view.MainView;
 import mvc.view.TicTacToeView;
 import mvc.view.VerschluesselungsView;
 
 public class Controller {
+	// Views
 	private MainView mainView;
 	private TicTacToeView ticTacToeView;
 	private VerschluesselungsView verschluesselungsView;
 	
+	//Models
+	private TextVerschluesseln verschluesselungsModel;
 	
 	public Controller(){
 		this.mainView = new MainView();
 		this.ticTacToeView = new TicTacToeView();
 		this.verschluesselungsView = new VerschluesselungsView();
+		this.verschluesselungsModel = new TextVerschluesseln(false);
 	}
 }
