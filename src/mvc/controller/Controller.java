2afa10a478c0d6196f504c5f734eb3a7b8613ecc
Sandro <sandro.trottmann@casasoft.ch>
2018-04-30 09:56:45 +0200

    asd

--- src/mvc/controller/Controller.java
+++ src/mvc/controller/Controller.java
@@ -1,9 +1,15 @@
 package mvc.controller;
 
 import mvc.model.TextVerschluesseln;
+import mvc.model.BenzinRechner;
+import mvc.model.dreieck3d;
+import mvc.model.TicTacToe;
+
 import mvc.view.MainView;
 import mvc.view.TicTacToeView;
 import mvc.view.VerschluesselungsView;
+import mvc.view.dreiDPunkteView;
+import mvc.view.BenzinRechnerView;
 
 public class Controller {
 	// Views
@@ -11,14 +17,24 @@ public class Controller {
 	private TicTacToeView ticTacToeView;
 	private VerschluesselungsView verschluesselungsView;
 	private dreiDPunkteView dreiDPunkteView;
+	private BenzinRechnerView benzinRechnerView;
 	
 	//Models
 	private TextVerschluesseln verschluesselungsModel;
+	private BenzinRechner benzinRechnerModel;
+	private dreieck3d dreiDPunkteModel;
+	private TicTacToe ticTacToeModel;
 	
 	public Controller(){
 		this.mainView = new MainView();
 		this.ticTacToeView = new TicTacToeView();
 		this.verschluesselungsView = new VerschluesselungsView();
+		this.dreiDPunkteView = new dreiDPunkteView();
+		this.benzinRechnerView = new BenzinRechnerView();
+		
 		this.verschluesselungsModel = new TextVerschluesseln(false);
+		this.benzinRechnerModel = new BenzinRechner();
+		this.dreiDPunkteModel = new dreieck3d();
+		this.ticTacToeModel = new TicTacToe();
 	}
 }
