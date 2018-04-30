1f7b626758e27b9cb67586292d5c2e845537c3e4
Janosch Diggelmann <janosch.digg@outlook.com>
2018-04-30 10:07:53 +0200

    try

--- src/mvc/view/dreiDPunkteView.java
+++ src/mvc/view/dreiDPunkteView.java
@@ -1,5 +1,20 @@
 package mvc.view;
 
-public class dreiDPunkteView {
+import java.io.IOException;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class dreiDPunkteView 
+{
+	public void start(Stage stage) throws IOException
+	{
+		 Parent root = FXMLLoader.load(getClass().getResource("3dpunkte.fxml"));
+        Scene scene = new Scene(root, 300, 275);
+        stage.setTitle("3D Punkte");
+        stage.setScene(scene);
+        stage.show();
+	}
 }
