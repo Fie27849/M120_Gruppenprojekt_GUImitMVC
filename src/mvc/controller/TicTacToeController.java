7401dd2a1b7ffdcedd05533234e8c14929ca0522
DenisTurkovic <d.turkovic00@gmx.ch>
2018-05-28 10:54:22 +0200

    Revert "tasd"
    This reverts commit 5ab8943d882617de3c9ef4f1680fa3db482d4a1b.

--- src/mvc/controller/TicTacToeController.java
+++ src/mvc/controller/TicTacToeController.java
@@ -1,11 +1,16 @@
 package mvc.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import mvc.model.TicTacToe;
 
 public class TicTacToeController extends MainController {
-
+	
 	private TicTacToe game;
-
+	
 	@FXML
 	private Button a1;
 	@FXML
@@ -26,34 +31,12 @@ public class TicTacToeController extends MainController {
 	private Button c3;
 	@FXML
 	private Label info;
-
+	
 	@FXML
 	public void handleButtonClick(ActionEvent event) {
-		switch (((Node) event.getSource()).getId()) {
-		case "a1":
-			break;
-		case "a2":
-			break;
-		case "a3":
-			break;
-		case "b1":
-			break;
-		case "b2":
-			break;
-		case "b3":
-			break;
-		case "c1":
-			break;
-		case "c2":
-			break;
-		case "c3":
-			break;
-		default:
-			break;
-		}
+		System.out.println(((Node) event.getSource()).getId());
 	}
 
-public class TicTacToeController {
 	@FXML
 	public void handleMenuClick() {
 		
