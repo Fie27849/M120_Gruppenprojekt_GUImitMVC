be1e1908f717d2096f8d6d6a9fb1e46fcad77930
Janosch Diggelmann <janosch.digg@outlook.com>
2018-05-28 16:04:54 +0200

    s
    s

--- src/mvc/controller/MainController.java
+++ src/mvc/controller/MainController.java
@@ -64,7 +64,7 @@ public class MainController {
 			this.TicTacToeView.changeScene(stage);
 			break;
 		case "textverschluesselung":
-			this.verschluesselungsView = new VerschluesselungsView("../view/textverschluesselung.fxml", "Text Verschl�sseln");
+			this.verschluesselungsView = new VerschluesselungsView("../view/textverschluesselung.fxml", "Text Verschlüsseln");
 			this.verschluesselungsView.changeScene(stage);
 			break;
 		case "benzinrechner":
