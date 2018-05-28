b1444d3606e52de32422c9702c0bd29551187bc4
DenisTurkovic <d.turkovic00@gmx.ch>
2018-05-28 10:43:07 +0200

    TTT

--- src/mvc/view/TicTacToeView.java
+++ src/mvc/view/TicTacToeView.java
@@ -1,9 +1,19 @@
 package mvc.view;
 
+import javafx.scene.control.Button;
+
 public class TicTacToeView extends view {
 
 	public TicTacToeView (String linktofxml, String Title) {
 		super(linktofxml, Title);
+	}
+	
+	public void draw_field(Button a1, Button a2, Button a3, Button b1, Button b2, Button b3, Button c1, Button c2, Button c3, String[][] field) {
+		
+	}
+	
+	public void setButtonText(Button btn, String text) {
+		btn.setText(text);
 	}
 
 }
