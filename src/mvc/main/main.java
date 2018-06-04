666ec6eb1149c13e06f03023041e2fcd61bc5bd3
Janosch Diggelmann <janosch.digg@outlook.com>
2018-06-04 15:04:53 +0200

    iconasd
    asdasd

--- src/mvc/main/main.java
+++ src/mvc/main/main.java
@@ -1,11 +1,10 @@
 package mvc.main;
 
+import java.awt.Toolkit;
 import java.io.File;
 
+import javax.swing.ImageIcon;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
